23b1368415a7b7becf7bc6691d0bb517aabec852
MasayaGoto <masaya.goto@gild.co.jp>
2022-04-22 00:39:42 +0900

    :wrench: フォーマット修正 (#9)

--- execution.sh
+++ execution.sh
@@ -1,10 +1,10 @@
 #!bin/bash
 
 if [ $# -eq 0 ]; then
-    echo "引数は必須です。"
-    echo "'. execution.sh commit'   ->  コミット前にコーディングチェックを行います。"
-    echo "'. execution.sh push'     ->  プッシュ前にコーディングチェックを行います。コミット前のチェックは行いません。"
-    return
+  echo "引数は必須です。"
+  echo "'. execution.sh commit'   ->  コミット前にコーディングチェックを行います。"
+  echo "'. execution.sh push'     ->  プッシュ前にコーディングチェックを行います。コミット前のチェックは行いません。"
+  exit 1
 fi
 
 if [ "$1" != "commit" ] && [ "$1" != "push" ]; then
